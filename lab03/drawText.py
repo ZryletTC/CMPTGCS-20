8ad786cb23b1b445eb81fc304f295d8b10994d75
Tyler Pennebaker <pennebaker@umail.ucsb.edu>
2016-04-21 11:03:07 -0700

    Start lab03

---
+++ lab03/drawText.py
@@ -0,0 +1,74 @@
+#drawText - draws text using Turtle graphics
+
+import turtle
+#from turtle import *
+
+def drawText(text):
+    t = turtle.Turtle()
+    #t.speed(0)
+    t.ht()
+    #turtle.tracer(0,0)
+    for i in range(0,len(text)):
+        char = text[i]
+    turtle.exitonclick()
+    
+"""
+def newLine(t):
+    
+
+def newCharacter(t):
+
+
+def drawCharA(t):
+
+def drawCharB(t):
+
+def drawCharC(t):
+
+def drawCharD(t):
+
+def drawCharE(t):
+
+def drawCharF(t):
+
+def drawCharG(t):
+
+def drawCharH(t):
+
+def drawCharI(t):
+
+def drawCharJ(t):
+
+def drawCharK(t):
+
+def drawCharL(t):
+
+def drawCharM(t):
+
+def drawCharN(t): #&&&
+
+def drawCharO(t):
+
+def drawCharP(t): #&&&
+
+def drawCharQ(t):
+
+def drawCharR(t): #&&&
+
+def drawCharS(t):
+
+def drawCharT(t): #&&&
+
+def drawCharU(t):
+
+def drawCharV(t):
+
+def drawCharW(t): 
+
+def drawCharX(t):
+
+def drawCharY(t): #&&&
+
+def drawCharZ(t):
+
+"""
